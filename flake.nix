ca5c8d8e63a05ad7c603b7f95a6b0dab8b043e4d
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2022-12-20 18:37:37 +0100

    NixOS updated to version 22.11 with Hyprland

--- flake.nix
+++ flake.nix
@@ -2,30 +2,25 @@
   description = "A flake for building my NixOS configuration";
   
   inputs = {
-    # now upgrading from 22.05 to 22.11
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    # nur.url = "github:nix-community/NUR";
-
     # musnix.url = "github:musnix/musnix";
-
+    # nur.url = "github:nix-community/NUR";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    # { inputs.nixpkgs.follows = "nixpkgs"; };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.05";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-
-    # hyprland = {
-    #   url = "github:vaxerski/Hyprland";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    unstable.url = "nixpkgs/nixos-unstable";
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      # build with your own instance of nixpkgs
+      inputs.nixpkgs.follows = "unstable";
+    };
   };
   
-  outputs = inputs @ { self, nixpkgs, nixos-hardware, home-manager  }:
+  outputs = inputs @ { self, nixpkgs, nixos-hardware, home-manager, unstable, hyprland }:
     let
       user = "ubunteous";
     in
@@ -37,17 +32,52 @@
 
           modules =
             [
-              ./hosts/configuration.nix
+              ############
+              #   HOST   #
+              ############
+              
+              ./hosts/main-host.nix
+              # ./hosts/wayland.nix # not operation yet
+              # ./hosts/minimal.nix
               ./hardware/hardware-configuration.nix
               inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14s-amd-gen1
               # nixos-hardware.nixosModules.lenovo-thinkpad-t14s-amd-gen1
 
+              ##############
+              #   CONFIG   #
+              ##############
+              
               inputs.home-manager.nixosModules.home-manager # make home manager available to configuration.nix
               {
                 # use system-level nixpkgs => saves nixpkgs evaluation, adds consistency, removes NIX_PATH dependency
                 home-manager.useGlobalPkgs = true;
               }
+
+
+              ################
+              #   HYPRLAND   #
+              ################
+
+              inputs.hyprland.nixosModules.default
+              { programs.hyprland.enable = true; }
             ];
         };
+
+        
+        # hyprland with home manager
+        # homeConfigurations.${user} = inputs.home-manager.lib.homeManagerConfiguration {
+        #   pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        #   modules = [
+        #     hyprland.homeManagerModules.default
+        #     {
+        #       wayland.windowManager.hyprland = {
+        #         enable = true;
+        #         # xwayland.enable = false;
+        #       };
+        #     }
+        #   ];
+        # };
+
+        
       };
 }
