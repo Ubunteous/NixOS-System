9c256f6c777b2ed30c7cc316e2e077ddb4aee8bf
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-01-12 19:25:32 +0000

    New Wayland Compositor: Hyprland

--- wm/hyprland.nix
+++ wm/hyprland.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, user, ... }:
 # imported from services.nix
 
 {
   # Enable the Sway Compositor.
   programs.hyprland.enable = true;
+
+  users.users.${user}.packages = with pkgs; [
+    hyprpaper
+  ];
 }
 
