4301446d5d2762e973fe916f066479e2eada605a
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-01-12 19:36:45 +0000

    Dunst priorities colors set

--- home/dunst.nix
+++ home/dunst.nix
@@ -4,19 +4,31 @@
 #   DUNST   #
 #############
 
+# custom picture
+# dunstify -i ~/Pictures/picture.png "Text after the picture"
+
+# urgency
+# dunstify -u low "Lower urgency than normal or critical"
+
+# time before disappearance
+# dunstify -t 1000 "Leaves in one second"
+
+# sound notification with bar
+# dunstify -h string:x-dunst-stack-tag:"Brightness" -h int:value:$(pamixer --get-volume-human) "Brightness"
+
 {
   home-manager.users.${user} = {
 
     # https://gvolpe.com/blog/xmonad-polybar-nixos/
     services.dunst = {
       enable = true;
-      iconTheme = {
-        # name = "Latte";
-        # package = pkgs.catppuccin-gtk;
-        name = "Adwaita";
-        package = pkgs.gnome3.adwaita-icon-theme;
-        # size = "32x32";
-      };
+      # iconTheme = {
+      #   name = "Papirus-Dark";
+      #   package = pkgs.papirus-icon-theme;
+      #   size = "32x32";
+      # name = "Adwaita";
+      # package = pkgs.gnome3.adwaita-icon-theme;
+      # };
       settings = {
         global = {
           # monitor = 0;
@@ -29,9 +41,16 @@
           line_height = 4;
           format = ''<b>%s</b>\n%b'';
           alignment = "center";
-          # corner_radius = 0;
+          corner_radius = 10;
+          width = 300;
           # class = "dock";
 
+          # Align icons left/right/off
+          # icon_position = "left";
+
+          # Scale larger icons down to this size, disable with  0
+          # max_icon_size = 32;
+          
           frame_color = "#8CAAEE";
           separator_color = "frame";
         };
@@ -39,17 +58,20 @@
         urgency_low = {
           background = "#303446";
           foreground = "#C6D0F5";
+          highlight = "#f5a97f"; # peach
         };
 
         urgency_normal = {
           background = "#303446";
           foreground = "#C6D0F5";
+          highlight = "#c6a0f6"; # mauve
         };
         
         urgency_critical = {
           background = "#303446";
           foreground = "#C6D0F5";
           frame_color = "#EF9F76";
+          highlight = "#ed8796"; # red
         };
       };
     };
