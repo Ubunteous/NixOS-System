d17174fee5a944e01dcbbcc72af524e75edf266c
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-04-24 14:43:48 +0100

    nix-shell to provide libraries required by Waveform

---
+++ nix-shell/waveform.nix
@@ -0,0 +1,15 @@
+# This provides the packages required by u-he plugin dialogs
+# Place this shell.nix in the correct directory and run:
+# nix-shell --run reaper # or another daw of your choice
+
+with import <nixpkgs> {};
+mkShell {
+  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+    libGL
+    freetype
+    alsa-lib
+    curl
+    libgccjit
+  ];
+  NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
+}
