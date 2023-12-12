586fb454c0b19f4e9960953cc54e4eb78be99544
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-12-12 17:04:05 +0100

    Removed extra comments (pairs) from Haskell file

--- programming/Haskell.nix
+++ programming/Haskell.nix
@@ -5,8 +5,10 @@
     packages = with pkgs; [
       # Haskell
       ghc
-      ## cabal-install # packet manager + build
-      ## stack # ghc + package manager + build/test/benchmark
+
+      # haskellPackages.kmonad # currently broken in 22.11
+      # cabal-install # packet manager + build
+      # stack # ghc + package manager + build/test/benchmark
     ];
   };
 }
