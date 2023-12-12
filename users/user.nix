0a88cf78443c81174f2ed99c488c143c7d59a8c4
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-12-12 16:53:54 +0100

    New user groups for sound and kmonad input

--- users/user.nix
+++ users/user.nix
@@ -4,13 +4,22 @@
   # add zsh to /etc/shells
   environment.shells = with pkgs; [ zsh ];
 
+  # potential fix starting from update to 23.05
+  programs.zsh.enable = true;
+
+  users.groups = { uinput = {}; }; # for KMonad
+  
   # Define a user account
   # Do not forget to set a password with ‘passwd’
 
   users.users.${user} = {
     isNormalUser = true;
     description = "${user}";
-    extraGroups = [ "networkmanager" "wheel" ];
+
+    # realtime audio for musnix
+    # uinput and input for kmonad and kanata
+    extraGroups = [ "networkmanager" "wheel" "realtime" "audio" "uinput" ];
+
     shell = pkgs.zsh; # set user's default shell
   };
 }
