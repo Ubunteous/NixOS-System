7d8fbaca35227237e90b21a604f26288a772e123
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-12-12 16:59:31 +0100

    Changed secondary file system from ntfs3 to ntfs

--- core/file-systems.nix
+++ core/file-systems.nix
@@ -1,9 +1,15 @@
 { config, pkgs, ... }:
 
+# To mount manually, use this command with the correct partition name (found with lsblk) and mount point:
+# sudo mount /dev/nvme0n1p4 /mnt/windows
+
 {
+  # Mount external drives (NTFS support)
+  boot.supportedFilesystems = [ "ntfs" ]; # lenovo yoga 7 fix (mount usb / hdd)
+
   fileSystems."/mnt/windows" = {
-    device = "/dev/nvme0n1p3";
-    fsType = "ntfs3"; # "ntfs";
-    options = ["rw" "uid=1000" "nofail"];
+    device = "/dev/nvme0n1p4";
+    fsType = "ntfs"; # "ntfs3";
+    options = ["rw" "uid=1000" "gid=1000" "dmask=007" "fmask=117" "nofail"];
   };
-}
+  }
