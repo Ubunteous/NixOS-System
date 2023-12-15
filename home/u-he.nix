48abf3dd5ddd75d9dfe3ccd1333e4500696b6bf4
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-12-15 17:28:55 +0100

    Changed daw xdg entry to start with u-he fix

---
+++ home/u-he.nix
@@ -0,0 +1,41 @@
+{ pkgs, home-manager, user, ... }:
+
+############
+#   U-HE   #
+############
+
+# You can see rofi's cache and list of desktop files in:
+# $HOME/.cache/rofi3.druncache
+# cockos-reaper.desktop
+# com.bitwig.BitwigStudio.desktop or bitwig-studio.desktop ?
+{
+  home-manager.users.${user} = {
+
+    xdg.desktopEntries = {
+
+      u-he-reaper = {
+        name = "REAPER";
+        genericName = "REAPER";
+
+        # u-he dialog fix
+        exec = "nix-shell .nix.d/nix-shell/u-he.nix --run reaper";
+
+        icon = "/home/${user}/.nix.d/home/icons/Reaper Logo.png";
+        terminal = false;
+        type = "Application";
+      };
+
+      u-he-bitwig = {
+        name = "Bitwig Studio";
+        genericName = "Bitwig Studio";
+
+        # u-he dialog fix
+        exec = "nix-shell .nix.d/nix-shell/u-he.nix --run bitwig-studio";
+
+        icon = "/home/${user}/.nix.d/home/icons/Bitwig Logo.png";
+        terminal = false;
+        type = "Application";
+      };
+    };
+  };
+}
