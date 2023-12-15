ceb68da120ed18c4346dd14e534678f487d3a191
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-12-15 17:38:19 +0100

    Scale keepassxc properly for improved readability

--- core/boot.nix
+++ core/boot.nix
@@ -39,16 +39,26 @@
   services.logind.lidSwitch = "lock"; # "lock", "suspend"
   # services.logind.lidSwitchExternalPower = ;
   # services.logind.lidSwitchDocked = ; # lid closed and another monitor added
-  
+
+  #############################
+  #   ENVIRONMENT VARIABLES   #
+  #############################
+
+  # make keepassxc and vlc fonts bigger
+  # environment.sessionVariables = {
+  environment.variables = {
+    "QT_SCALE_FACTOR" = "1.25";
+  };
+
   #########################
   #   Lenovo Yoga 7 Fix   #
   #########################
-  
+
   ### Add <nixos-hardware/lenovo/thinkpad/t14s/amd/gen1> to imports = [];
   ### Or uncomment the following lines to fix wifi on the lenovo yoga 7	
 
   # Wifi support
-  #hardware.firmware = [ pkgs.rtw89-firmware ];
+  # hardware.firmware = [ pkgs.rtw89-firmware ];
 
   # For mainline support of rtw89 wireless networking
   #boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "5.16") pkgs.linuxPackages_latest;
