5a2bba7b1af3aa45d3eca7ca5040654034e636d3
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-03-07 00:14:40 +0100

    Moved monitoring configurations to dedicated directory

---
+++ lab/monitoring/prometheus.nix
@@ -0,0 +1,45 @@
+{ config, lib, ... }:
+
+# see https://xeiaso.net/blog/prometheus-grafana-loki-nixos-2020-11-20/
+
+with lib;
+let
+  cfg = config.lab.prometheus;
+  labcfg = config.lab;
+  in {
+    options.lab.prometheus = {
+      enable = mkEnableOption "Enables support for Prometheus";
+    };
+
+    config = mkIf (labcfg.enable && cfg.enable) {
+
+    ##############
+    # PROMETHEUS #
+    ##############
+
+    services.prometheus = {
+      enable = true;
+      # http://localhost:9090/
+      # port = 9090; # default is 9090
+
+      exporters = {
+        node = {
+          enable = true;
+          enabledCollectors = [ "systemd" ];
+          port = 9091;
+        };
+      };
+
+      # scrapeConfigs = [{
+      #   job_name = "chrysalis";
+      #   static_configs = [{
+      #     targets = [
+      #       "127.0.0.1:${
+      #         toString config.services.prometheus.exporters.node.port
+      #       }"
+      #     ];
+      #   }];
+      # }];
+    };
+  };
+}
