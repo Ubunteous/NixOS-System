bf985381faa05c368e68d04f800f32eba451ca98
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-03-28 17:58:26 +0100

    Moved break timer logic to its own script

---
+++ files/eww/scripts/break.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+dunstify -u critical -t 30000 'It is time for a break'
+
+# if [ $(eww get hide-first) = 'yes' ]; then
+#     eww update hide-first='no';
+#     # exit 0;
+# elif [ $(eww get break-needed) = 'yes' ]; then
+#     dunstify -u critical -t 30000 'It is time for a break'
+# fi
+
