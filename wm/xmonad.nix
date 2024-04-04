e6cb663bc5feb408323d1eb63c039055ecdc93f6
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-04-04 21:40:39 +0200

    Add _JAVA_AWT_WM_NONREPARENTING to xmonad for Java GUI

--- wm/xmonad.nix
+++ wm/xmonad.nix
@@ -4,21 +4,25 @@ with lib;
 let
   cfg = config.wm.xmonad;
   wmcfg = config.wm;
-in
-{
+in {
   options.wm.xmonad = {
     enable = mkEnableOption "Enables support for the XMonad windows manager";
   };
 
   config = mkIf (wmcfg.enable && cfg.enable) {
+    # fix issue with java apps not displaying properly in XMonad
+    environment.sessionVariables._JAVA_AWT_WM_NONREPARENTING = "1";
+
     # Enable the XMonad Window Manager.
     # services.xserver.displayManager.defaultSession = "none+xmonad";
     services.xserver.windowManager.xmonad = {
       enable = true;
       enableContribAndExtras = true;
-      extraPackages =
-        haskellPackages: [
-          haskellPackages.xmonad
+      extraPackages = haskellPackages:
+        with haskellPackages;
+        [
+          xmonad
+
           # haskellPackages.xmobar
           # haskellPackages.xmonad-contrib
           # haskellPackages.xmonad-extras
