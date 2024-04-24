10ea8839fd4a8c16d4fc2ef2df39726944facc3b
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-04-24 20:50:33 +0200

    Add lsp, treesit, formatter and linter to Clojure

--- programming/Clojure.nix
+++ programming/Clojure.nix
@@ -7,7 +7,7 @@ let
 in {
   options.languages.clojure = {
     enable =
-      mkEnableOption "Enables support for the Clojure programming languages";
+      mkEnableOption "Enables support for the Clojure programming language";
   };
 
   config = mkIf (langcfg.enable && cfg.enable) {
@@ -18,13 +18,8 @@ in {
         clojure-lsp
         cljfmt
         clj-kondo # => linter (included in clojure-lsp)
-        # joker # interpreter, linter and formatter
 
-        # build automation and dependency management
-        # leiningen
-
-        # scripting
-        # babashka
+        babashka # also adds #!bb shebang
       ];
     };
   };
