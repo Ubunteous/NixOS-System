fbcc184519ccf41adc432929e37a8c9c0ac88e53
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-04-24 20:50:46 +0200

    Add lsp, treesit, formatter and linter to Godot

--- programming/Godot.nix
+++ programming/Godot.nix
@@ -4,18 +4,13 @@ with lib;
 let
   cfg = config.languages.godot;
   langcfg = config.languages;
-  in
-  {
-    options.languages.godot = {
-      enable = mkEnableOption "Enables support for the Gdscript programming languages";
-    };
+in {
+  options.languages.godot = {
+    enable =
+      mkEnableOption "Enables support for the Gdscript programming language";
+  };
 
-    config = mkIf (langcfg.enable && cfg.enable) {
-      users.users.${user} = {
-	packages = with pkgs; [
-	  godot_4
-	  gdtoolkit
-	];
-      };
-    };
-  }
+  config = mkIf (langcfg.enable && cfg.enable) {
+    users.users.${user} = { packages = with pkgs; [ godot_4 gdtoolkit ]; };
+  };
+}
