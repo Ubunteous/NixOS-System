028d5bb16c86ed10a127dce8209d5d7dc6f09ab3
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-04-24 20:51:41 +0200

    Add lsp, treesit, formatter and linter to Javascript

--- programming/Javascript.nix
+++ programming/Javascript.nix
@@ -4,25 +4,42 @@ with lib;
 let
   cfg = config.languages.javascript;
   langcfg = config.languages;
-  in {
-    options.languages.javascript = {
-      enable =
-	mkEnableOption "Enables support for the Javascript programming languages";
-  };
+in {
+  options.languages.javascript = {
+    enable =
+      mkEnableOption "Enables support for the Javascript programming language";
 
-  config = mkIf (langcfg.enable && cfg.enable) {
-    users.users.${user} = {
-      packages = with pkgs; [
-        # nodePackages.nodejs
-        # nodePackages.create-react-app
-
-        # biome # lint/format => does not send to stdout
-        nodePackages.typescript-language-server
-        nodePackages.prettier
-
-        nodejs
-        create-react-app
-      ];
+    addTypescript = mkOption {
+      type = types.bool;
+      default = false;
+      description =
+        lib.mdDoc "Enables support for the Typescript programming language";
     };
   };
+
+  config = mkMerge [
+    (mkIf (langcfg.enable && cfg.enable) {
+      users.users.${user} = {
+        packages = with pkgs; [
+          # yarn2nix
+
+          # biome # lint/format => does not send to stdout
+          nodePackages.typescript-language-server
+          nodePackages.prettier
+          nodePackages.eslint
+
+          # jupyter. setup with ijsinstall command
+          nodePackages.ijavascript
+
+          nodejs
+          create-react-app
+        ];
+      };
+    })
+    (mkIf (langcfg.enable && cfg.addTypescript) {
+      users.users.${user} = {
+        packages = with pkgs; [ typescript nodePackages.ts-node ];
+      };
+    })
+  ];
 }
