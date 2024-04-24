c6d6cf0cfe49649149d1e0b7855e3ef2d83e031a
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-04-24 20:51:01 +0200

    Add lsp, treesit, formatter and linter to Haskell

--- programming/Haskell.nix
+++ programming/Haskell.nix
@@ -4,28 +4,28 @@ with lib;
 let
   cfg = config.languages.haskell;
   langcfg = config.languages;
-  in {
-    options.languages.haskell = {
-      enable =
-	mkEnableOption "Enables support for the Haskell programming languages";
-    };
+in {
+  options.languages.haskell = {
+    enable =
+      mkEnableOption "Enables support for the Haskell programming language";
+  };
 
-    config = mkIf (langcfg.enable && cfg.enable) {
-      users.users.${user} = {
-	packages = with pkgs; [
-          ghc
+  config = mkIf (langcfg.enable && cfg.enable) {
+    users.users.${user} = {
+      packages = with pkgs; [
+        ghc
 
-          # can fail with incorrect hs code (and different dependencies - ghc)
-          # seems to run once and end successfully which stops eglot
-          haskell-language-server
+        # can fail with incorrect hs code (and different dependencies - ghc)
+        # seems to run once and end successfully which stops eglot
+        haskell-language-server
 
-          hlint
-          ormolu
-          # stylish-haskell
-          # haskellPackages.fourmolu # configurable branch of ormolu
+        hlint
+        ormolu
+        # stylish-haskell
+        # haskellPackages.fourmolu # configurable branch of ormolu
 
-          # haskellPackages.kmonad # currently broken in 22.11
-          # cabal-install # packet manager + build
+        # haskellPackages.kmonad # currently broken in 22.11
+        # cabal-install # packet manager + build
         # stack # ghc + package manager + build/test/benchmark
       ];
     };
