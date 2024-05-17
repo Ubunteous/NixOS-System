39c7318b6c02cb64803cb2bdb1929cc69aadc787
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-05-17 10:20:44 +0200

    New thunderbird config for mail management

--- home/mail/thunderbird.nix
+++ home/mail/thunderbird.nix
@@ -2,11 +2,11 @@
 
 with lib;
 let
-  cfg = config.mail.thunderbird;
-  homecfg = config.mail;
+  cfg = config.home.mail.thunderbird;
+  homecfg = config.home.mail;
 in {
 
-  options.mail.thunderbird = {
+  options.home.mail.thunderbird = {
     enable = mkEnableOption "Enables support for thunderbird";
   };
 
@@ -20,7 +20,7 @@ in {
       # };
 
       profiles."m9" = {
-        name = "${user}";
+        # name = "m9";
 
         isDefault = true;
 
@@ -33,5 +33,37 @@ in {
         # withExternalGnupg = false;
       };
     };
+
+    # OPTIONS:
+    # USE DDG instead of GOOGLE
+    # FONT SIZE: BIGGER (24/13)
+    # UNTICK show only display name for people in my address book
+    # INCOMING MAIL do not play a sound
+    # SYSTEM INTEGRATION DO not check that firebird is the default
+
+    # WEB CONTENT DO NOT SAVE COOKIES
+    # DO NOT SEND TECH DATA TO MOZILLA
+    # CHAT NOTIF DO NOT PLAY A SOUND
+
+    # SORT MAIL BY DATE DESCENDING
+    # VIEW DENSITY MAX
+    # UI FONT
+
+    # WHITE TEXT THEME BLACK ON BLACK background
+    # Bottom of config > General > layout.css.devPixelsPerPx > 1.5
+
+    # accounts.email = {
+    #   accounts.m9 = {
+    #     primary = true;
+
+    #     # address = config.services.maildata.address;
+    #     # userName = config.services.maildata.address;
+    #     # realName = config.services.maildata.name;
+
+    #     # imap.host = "imap.gmail.com";
+    #     # smtp.host = "smtp.gmail.com";
+    #   };
+    # };
+
   };
 }
