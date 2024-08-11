54fce25765503a339c099b3caca146cca2b86366
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-08-11 11:58:32 +0200

    Switching from nil lsp to nixd

--- programming/Nix.nix
+++ programming/Nix.nix
@@ -4,18 +4,19 @@ with lib;
 let
   cfg = config.languages.nix;
   langcfg = config.languages;
-  in {
-    options.languages.nix = {
+in {
+  options.languages.nix = {
     enable = mkEnableOption "Enables support for the nix programming language";
-    };
+  };
 
   config = mkIf (langcfg.enable && cfg.enable) {
     users.users.${user} = {
       packages = with pkgs; [
-        nil # lsp
-        # nixd # lsp. less mature than nil
+        # nil # lsp > light
+        nixd # lsp > powerfull
 
-        nixfmt # formatter
+        # nixfmt # for the new RFC 166-style formatter
+        nixfmt-classic # formatter
         # alejandra # formatter
 
         deadnix # find unused vars/params
@@ -27,6 +28,29 @@ let
         # disabled = ["repeated_keys"]
         # nix_version = '2.4'
         # ignore = ['.direnv', "packages/nodePackages/node-env.nix", "npins/default.nix"]
+
+        #########
+        # TOOLS #
+        #########
+
+        # see configuration options and dependencies
+        # nix-inspect # nix-inspect -p ~/.nix.d
+        # nix-tree
+
+        # visualise vc-roots to delete
+        # nix-du
+
+        # index generation freezes computer
+        # nix-index # nix-locate command
+
+        # generate package from url
+        # nix-init
+
+        # pipe build for extra info
+        # nix-output-monitor # nom-command
+
+        # documentation
+        # manix
       ];
     };
   };
