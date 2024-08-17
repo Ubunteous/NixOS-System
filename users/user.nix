501500b9b062d84e2298626038495c8ccf67c41d
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-08-17 16:29:39 +0200

    User converted to enable pattern

--- users/user.nix
+++ users/user.nix
@@ -1,25 +1,43 @@
-{ config, pkgs, user, ... }:
+{ config, lib, pkgs, user, ... }:
 
-{
-  # add zsh to /etc/shells
-  environment.shells = with pkgs; [ zsh ];
+with lib;
+let
+  cfg = config.user.laptop;
+  usercfg = config.user;
+in {
+  options.user.laptop = { enable = mkEnableOption "Add ubunteous user"; };
 
-  # potential fix starting from update to 23.05
-  programs.zsh.enable = true;
+  config = mkIf (usercfg.enable && cfg.enable) {
+    # add zsh to /etc/shells
+    environment.shells = with pkgs; [ zsh ];
 
-  users.groups = { uinput = {}; }; # for KMonad
-  
-  # Define a user account
-  # Do not forget to set a password with ‘passwd’
+    # potential fix starting from update to 23.05
+    programs.zsh.enable = true;
 
-  users.users.${user} = {
-    isNormalUser = true;
-    description = "${user}";
+    users.groups = { uinput = { }; }; # for KMonad
 
-    # realtime audio for musnix
-    # uinput and input for kmonad and kanata
-    extraGroups = [ "networkmanager" "wheel" "realtime" "audio" "uinput" ];
+    # Define a user account
+    # Do not forget to set a password with ‘passwd’
+    users.users.${user} = {
+      isNormalUser = true;
+      description = "${user}";
 
-    shell = pkgs.zsh; # set user's default shell
+      # realtime audio for musnix
+      # uinput and input for kmonad and kanata
+      extraGroups = [ "networkmanager" "wheel" "realtime" "audio" "uinput" ];
+
+      shell = pkgs.zsh; # set user's default shell
+
+      # openssh = {
+      #   authorizedPrincipals = [ "example@host" ];
+      #   authorizedKeys.keys = [ "keys" ];
+      #   authorizedKeys.keyFiles = [ "paths" ];
+      # };
+
+      # THESE ARE ALL DEFAULTS
+      # createHome = true;
+      # home = "/home/${user}";
+      # homeMode = "700"; # defaults to "700"
+    };
   };
 }
