19e1a76cc02f4706e30ace8b736c44d46702449d
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-10-18 23:10:39 +0200

    Bitwig studio now has configuration from nixpgks

--- pkgs/bitwig3.nix
+++ pkgs/bitwig3.nix
@@ -1,22 +1,19 @@
 { stdenv, fetchurl, alsa-lib, cairo, dpkg, freetype, gdk-pixbuf, glib, gtk3, lib
 , xorg, libglvnd, libjack2, ffmpeg, libxkbcommon, xdg-utils, zlib, pulseaudio
-, wrapGAppsHook, makeWrapper }:
-
-# error: libxkbcommon_7 has been removed because it has security issues and not used in nixpkgs, move to 'libxkbcommon'
+, wrapGAppsHook3, makeWrapper }:
 
 stdenv.mkDerivation rec {
   pname = "bitwig-studio";
-  version = "3.1.3"; # mouse cursor icon broken since 23.11
-  # version = "3.2.8"; # audio engine broken in this version
+
+  version = "3.1.3";
 
   src = fetchurl {
     url =
       "https://downloads.bitwig.com/stable/${version}/${pname}-${version}.deb";
-    sha256 = "11z5flmp55ywgxyccj3pzhijhaggi42i2pvacg88kcpj0cin57vl"; # 3.1.3
-    # sha256 = "18ldgmnv7bigb4mch888kjpf4abalpiwmlhwd7rjb9qf6p72fhpj"; # 3.2.8
+    sha256 = "11z5flmp55ywgxyccj3pzhijhaggi42i2pvacg88kcpj0cin57vl";
   };
 
-  nativeBuildInputs = [ dpkg makeWrapper wrapGAppsHook ];
+  nativeBuildInputs = [ dpkg makeWrapper wrapGAppsHook3 ];
 
   unpackCmd = ''
     mkdir -p root
@@ -47,22 +44,15 @@ stdenv.mkDerivation rec {
     stdenv.cc.cc.lib
   ];
 
-  # binPath = lib.makeBinPath [
-  #   xdg-utils ffmpeg
-  # ];
-
   ldLibraryPath = lib.strings.makeLibraryPath buildInputs;
 
   installPhase = ''
     runHook preInstall
-        
+
     mkdir -p $out/bin
     cp -r opt/bitwig-studio $out/libexec
     ln -s $out/libexec/bitwig-studio $out/bin/bitwig-studio
     cp -r usr/share $out/share
-    substitute usr/share/applications/bitwig-studio.desktop \
-      $out/share/applications/bitwig-studio.desktop \
-      --replace /usr/bin/bitwig-studio $out/bin/bitwig-studio
 
     runHook postInstall
   '';
@@ -84,11 +74,7 @@ stdenv.mkDerivation rec {
         --prefix PATH : "${lib.makeBinPath [ ffmpeg ]}" \
         --suffix PATH : "${lib.makeBinPath [ xdg-utils ]}"
     done
-
   '';
-  # --prefix PATH : "${binPath}" \
-
-  # --set LD_PRELOAD "${libxkbcommon.out}/lib/libxkbcommon.so" || true
 
   meta = with lib; {
     description = "A digital audio workstation";
@@ -100,6 +86,6 @@ stdenv.mkDerivation rec {
     homepage = "https://www.bitwig.com/";
     license = licenses.unfree;
     platforms = [ "x86_64-linux" ];
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ bfortz michalrus mrVanDalo ];
   };
 }
