b7a1e9c9392a4c1e2f5d15a46cc40576c5e74f76
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-11-06 22:25:30 +0100

    Cleaned flameshot config

--- home/flameshot.nix
+++ home/flameshot.nix
@@ -4,16 +4,19 @@ with lib;
 let
   cfg = config.home.flameshot;
   homecfg = config.home;
-in
-{
-  options.home.flameshot = {
-    enable = mkEnableOption "Enable support for Flameshot";
+  in {
+    options.home.flameshot = {
+      enable = mkEnableOption "Enable support for Flameshot";
   };
 
   config = mkIf (homecfg.enable && cfg.enable) {
     services.flameshot = {
       enable = true;
-      settings = { General = { savePath = "${config.home.homeDirectory}/Pictures/Screenshots"; }; };
+      settings = {
+        General = {
+          savePath = "${config.home.homeDirectory}/Pictures/Screenshots";
+        };
+      };
     };
   };
 }
