280ea00fad8e29eda18bf87b32ecceafd45183ff
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-11-15 15:30:34 +0100

    New initial config for navidrome

---
+++ lab/self-hosted/navidrome.nix
@@ -0,0 +1,28 @@
+{ config, lib, ... }:
+
+with lib;
+let
+  cfg = config.lab.navidrome;
+  corecfg = config.lab;
+in {
+
+  options.lab.unbound = {
+    enable = mkEnableOption "Enables support for Navidrome";
+  };
+
+  config = mkIf (corecfg.enable && cfg.enable) {
+    services.navidrome = {
+      enable = true;
+
+      openFirewall = true;
+
+      # settings = {
+      # 	Port = 4533;
+      # 	Address = "127.0.0.1";	
+      # };
+
+      # user = "navidrome";
+      # group = "navidrome";
+    };
+  };
+}
