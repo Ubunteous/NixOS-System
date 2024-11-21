ad90db7b692d32188eae35f3b9ff2f611a86589d
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-11-21 19:31:37 +0100

    Added documentation to qtile.nix

--- wm/qtile.nix
+++ wm/qtile.nix
@@ -4,14 +4,28 @@ with lib;
 let
   cfg = config.wm.qtile;
   wmcfg = config.wm;
-in
-{
+in {
   options.wm.qtile = {
-    enable = mkEnableOption "Enables support for the Qtile windows manager (also exists as a Wayland compositor)";
+    enable = mkEnableOption
+      "Enables support for the Qtile windows manager (also exists as a Wayland compositor)";
   };
 
   config = mkIf (wmcfg.enable && cfg.enable) {
-    # services.xserver.displayManager.defaultSession = "none+qtile";
+
+    # nixpkgs.overlays = [
+    #   (final: prev: {
+    #     qtile-unwrapped = prev.qtile-unwrapped.overrideAttrs (new: old: {
+    #       passthru.providedSessions = [ "qtile" "qtile-wayland" ];
+    #       postPatch = old.postPatch + ''
+    #         mkdir -p $out/share/wayland-sessions
+    #         mkdir -p $out/share/xsessions
+    #         install resources/qtile.desktop -Dt $out/share/xsessions
+    #         install resources/qtile-wayland.desktop -Dt $out/share/wayland-sessions
+    #       '';
+    #     });
+    #   })
+    # ];
+
     services.xserver.windowManager.qtile = {
       enable = true;
 
@@ -22,7 +36,8 @@ in
       #   qtile-extras
       # ];
 
-      backend = "x11"; # "wayland"
+      # either x11 or wayland. check with echo $XDG_SESSION_TYPE
+      backend = config.wm.display_backend;
     };
   };
 }
