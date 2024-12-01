cffd533b4f823b916c81bab075f661ad614e92b1
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-12-01 22:26:31 +0100

    Added more wm options for x11/wayland and wm

--- wm/default.nix
+++ wm/default.nix
@@ -1,12 +1,23 @@
-{ config, lib, ... }:
+{ lib, ... }:
 
-with lib;
-{
-  options.wm.enable = mkEnableOption "Window Managers";
+with lib; {
+  options.wm = {
+    enable = mkEnableOption "Window Managers";
+
+    main = mkOption {
+      type = lib.types.str;
+      description = "Main Window Managers";
+    };
+
+    display_backend = mkOption {
+      type = lib.types.enum [ "x11" "wayland" ];
+      description = "Either X11 or Wayland";
+    };
+  };
 
   imports = [
     # current Desktop Environment or Windows Manager 
-    ./cinnamon.nix # => has a built-in bar 
+    ./cinnamon.nix # => has a built-in bar
     ./xmonad.nix # => can use xmobar or taffybar
 
     ./leftwm.nix # => must use polybar
