21a529fcd3540c1a092d9879b90c36e8de1b1ec6
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-01-12 22:07:33 +0100

    New ssh config

---
+++ home/terminal/ssh.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, user, ... }:
+
+with lib;
+let
+  cfg = config.home.terminal.ssh;
+  homecfg = config.home;
+in {
+  options.home.terminal.ssh = {
+    enable = mkEnableOption "Enable support for SSH";
+  };
+
+  config = mkIf (homecfg.enable && cfg.enable) {
+    ###########
+    #   SSH   #
+    ###########
+
+    programs.ssh = {
+      enable = true;
+
+      # Configure sharing of multiple sessions over a single network connection
+      # re-use existing connection if possible (faster+stable)
+      controlMaster = "auto";
+      controlPersist = "yes";
+      controlPath = "~/.ssh/control/%C"; # default: "~/.ssh/master-%r@%n:%p"
+    };
+
+    # services.ssh-agent.enable = true;
+    # services.gpg-agent.enableSshSupport = true;
+  };
+}
