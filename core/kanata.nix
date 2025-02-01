28158ebd0ab886caa6f35846a2632ab3f4511fc1
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-02-01 23:30:35 +0100

    Make kanata config for kbd67

--- core/kanata.nix
+++ core/kanata.nix
@@ -16,29 +16,37 @@ in {
       # default: pkgs.kanata
       # package = pkgs.kanata-with-cmd;
 
-      keyboards."laptop" = {
-        # Paths to keyboard devices
-        devices = [ "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ];
+      keyboards = {
+        "laptop" = {
+          # Paths to keyboard devices
+          devices = [ "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ];
 
-        config = builtins.readFile ../files/keyboard/kanata.txt;
+          config = builtins.readFile ../files/keyboard/kanata.txt;
 
-        # defcfg conf other than linux-dev and linux-continue-if-no-devs-found (set to yes)
-        # too early: concurrent-tap-hold yes
-        extraDefCfg = ''
-          danger-enable-cmd yes      
-        '';
+          # defcfg conf other than linux-dev and linux-continue-if-no-devs-found (set to yes)
+          # too early: concurrent-tap-hold yes
+          # extraDefCfg = ''
+          #   danger-enable-cmd yes      
+          # '';
 
-        # process-unmapped-keys yes
-        # sequence-timeout 2000
+          # process-unmapped-keys yes
+          # sequence-timeout 2000
 
-        # visible-backspaced, hidden-suppressed or hidden-delay-type
-        # sequence-input-mode visible-backspaced
+          # visible-backspaced, hidden-suppressed or hidden-delay-type
+          # sequence-input-mode visible-backspaced
 
-        # sequence-backtrack-modcancel no
-        # log-layer-changes no
-        # delegate-to-first-layer yes
+          # sequence-backtrack-modcancel no
+          # log-layer-changes no
+          # delegate-to-first-layer yes
 
-        # extraArgs = [ "..." ];
+          # extraArgs = [ "..." ];
+        };
+        "kbd" = {
+          devices =
+            [ "/dev/input/by-id/usb-KBDfans_kbd67mkiirgb_v3-event-kbd" ];
+
+          config = builtins.readFile ../files/keyboard/kbd67.txt;
+        };
       };
     };
   };
