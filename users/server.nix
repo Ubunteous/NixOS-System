ea6b01b310d53ba0abc206848e5754cc74e18656
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-02-01 23:30:54 +0100

    Server packages

--- users/server.nix
+++ users/server.nix
@@ -14,16 +14,32 @@ in {
     # potential fix starting from update to 23.05
     programs.zsh.enable = true;
 
-    users.groups.${user} = {};
+    users.groups.${user} = { };
 
     users.users.${user} = {
       isNormalUser = true;
       description = "${user}";
-      group = "${user}"; # for safety as nogroup is not ideal 
+      group = "${user}"; # for safety as nogroup is not ideal
 
       extraGroups = [ "networkmanager" "wheel" "uinput" ];
 
       shell = pkgs.zsh; # set user's default shell
+
+      packages = with pkgs; [
+        file-roller # file-roller
+        gnome-disk-utility # gnome-disks
+        keepassxc
+        networkmanagerapplet
+        zip
+        eza
+        gdu # disk usage
+        just # make
+        ripgrep # grep. combine it with fzf later
+        tlp # battery life
+        eww
+        # eww-wayland # wayland variant
+        rofi # history sorted by frequency in ~/.cache/rofi3.(d)runcache
+      ];
     };
   };
 }
