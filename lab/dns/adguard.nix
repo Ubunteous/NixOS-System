7fd5f59da0b6c3c9204f28cabfed70c5d59c7afd
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-02-04 22:25:37 +0100

    DNS setup complete

--- lab/dns/adguard.nix
+++ lab/dns/adguard.nix
@@ -14,10 +14,44 @@ in {
     services.adguardhome = {
       enable = true;
 
+      settings = {
+        # http = { address = "127.0.0.1:3000"; };
+
+        dns = {
+          upstream_dns = [
+            # # Example config with quad9
+            # "9.9.9.9#dns.quad9.net"
+            # "149.112.112.112#dns.quad9.net"
+
+            # # default
+            # "https://dns10.quad9.net/dns-query"
+
+            # Unbound
+            "127.0.0.1:5335"
+          ];
+        };
+
+        filtering = {
+          protection_enabled = true;
+          filtering_enabled = true;
+
+          parental_enabled = false;
+          safe_search.enabled = false;
+        };
+
+        filters = map (url: {
+          enabled = true;
+          url = url;
+        }) [
+          "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt" # list of Hacked Malware Web Sites
+          "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt" # malicious url blocklist
+        ];
+      };
+
       # extraArgs = [];
       # allowDHCP = config.services.adguardhome.settings.dhcp.enabled or false
 
-      # openFirewall = true;
+      openFirewall = true;
 
       # allow changes on the web interface to be saved
       # mutableSettings = false;
--- lab/dns/unbound.nix
+++ lab/dns/unbound.nix
@@ -14,34 +14,76 @@ in {
     services.unbound = {
       enable = true;
 
-      # # example for settings
-      # settings = {
-      #   server = { interface = [ "127.0.0.1" ]; };
-      #   forward-zone = [
-      #     {
-      #       name = ".";
-      #       forward-addr = "1.1.1.1@853#cloudflare-dns.com";
-      #     }
-      #     {
-      #       name = "example.org.";
-      #       forward-addr = [
-      #         "1.1.1.1@853#cloudflare-dns.com"
-      #         "1.0.0.1@853#cloudflare-dns.com"
-      #       ];
-      #     }
-      #   ];
-      #   remote-control.control-enable = true;
-      # };
+      # example for settings
+      settings = {
+        # When only using Unbound as DNS, make sure to replace 127.0.0.1 with your ip address
+        # When using Unbound in combination with pi-hole or Adguard, leave 127.0.0.1, and point Adguard to 127.0.0.1:PORT
+        server = {
+          interface = [ "127.0.0.1" ];
+          port = 5335;
+          access-control = [ "127.0.0.1 allow" ];
 
-      # user = "unbound";
-      # group = "unbound";
-      # resolveLocalQueries = true;
-      # enableRootTrustAnchor = true;
-      # stateDir = "/var/lib/unbound";
-      # package = pkgs.unbound-with-systemd;
+          # Based on recommended settings in https://docs.pi-hole.net/guides/dns/unbound/#configure-unbound
+          harden-glue = true;
+          harden-dnssec-stripped = true;
+          use-caps-for-id = false;
+          prefetch = true;
+          edns-buffer-size = 1232;
+          # logfile = "/var/log/unbound";
 
-      # localControlSocketPath = null; # "/run/unbound/unbound.ctl"
-      # checkconf = "!services.unbound.settings ? include && !services.unbound.settings ? remote-control";
+          # Custom settings
+          hide-identity = true;
+          hide-version = true;
+
+          # send minimal amount of information to upstream servers to enhance privacy
+          qname-minimisation = true;
+        };
+
+        forward-zone = [
+          # {
+          #   name = ".";
+          #   forward-addr = "1.1.1.1@853#cloudflare-dns.com";
+          # }
+          {
+            name = ".";
+            forward-addr =
+              [ "9.9.9.9#dns.quad9.net" "149.112.112.112#dns.quad9.net" ];
+            forward-tls-upstream = true; # Protected DNS
+          }
+          {
+            name = "example.org.";
+            forward-addr = [
+              "1.1.1.1@853#cloudflare-dns.com"
+              "1.0.0.1@853#cloudflare-dns.com"
+            ];
+          }
+          {
+            name = "example.com.";
+            forward-addr = [
+              "1.1.1.1@853#cloudflare-dns.com"
+              "1.0.0.1@853#cloudflare-dns.com"
+            ];
+          }
+        ];
+
+        remote-control.control-enable = true;
+      };
+
+      localControlSocketPath = null; # "/run/unbound/unbound.ctl"
+      # checkconf = true;
+
+      ############
+      # DEFAULTS #
+      ############
+
+      user = "unbound";
+      group = "unbound";
+      resolveLocalQueries = true;
+      enableRootTrustAnchor = true;
+      stateDir = "/var/lib/unbound";
+
+      # package = pkgs.unbound; # pkgs.unbound-with-systemd
+
     };
   };
 }
