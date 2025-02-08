931143c323295493d62fad9934a113452877d82d
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-02-08 12:30:22 +0100

    Improving adguard config

--- lab/dns/adguard.nix
+++ lab/dns/adguard.nix
@@ -4,6 +4,7 @@ with lib;
 let
   cfg = config.lab.adguard;
   labcfg = config.lab;
+  server-address = "192.168.1.99";
 in {
 
   options.lab.adguard = {
@@ -29,6 +30,10 @@ in {
             # Unbound
             "127.0.0.1:5335"
           ];
+
+          # all_servers = true;
+          # bootstrap_dns = [ "x.x.x.x" ];
+          bind_hosts = [ "127.0.0.1" server-address ];
         };
 
         filtering = {
@@ -36,7 +41,18 @@ in {
           filtering_enabled = true;
 
           parental_enabled = false;
-          safe_search.enabled = false;
+          safe_search.enabled = true;
+
+          rewrites = [
+            {
+              domain = "*.mydomain.com";
+              answer = server-address;
+            }
+            {
+              domain = "mydomain.com";
+              answer = server-address;
+            }
+          ];
         };
 
         filters = map (url: {
@@ -54,10 +70,11 @@ in {
       openFirewall = true;
 
       # allow changes on the web interface to be saved
-      # mutableSettings = false;
+      mutableSettings = true;
 
       # port = 3090; # defaults to 3000
       # host = "127.0.0.1"; # defaults to "0.0.0.0";
+      host = "0.0.0.0";
     };
   };
 }
