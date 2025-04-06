119f0060940dedc532c6673b0f3b3510a5b2b96c
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-04-06 22:23:40 +0200

    Sanoid setup for later

--- lab/backup/sanoid.nix
+++ lab/backup/sanoid.nix
@@ -7,50 +7,71 @@ let
 in {
 
   options.lab.sanoid = {
-    enable = mkEnableOption "Enables support for sanoid";
+    enable = mkEnableOption
+      "Enables support for sanoid (which comes bundled with syncoid)";
   };
+
+  ###########
+  # syncoid #
+  ###########
+
+  # syncoid data/images/vm root@remotehost:backup/images/vm # ssh share
+  # -r recursive
+  # --compress # for network transfer
+  # --skip-parent
+  # --exclude-datasets=REGEX
+  # --include/exclude-snaps=REGEX
 
   config = mkIf (labcfg.enable && cfg.enable) {
     services.sanoid = {
       enable = true;
 
-      templates."name" = {
-        # how many snapshots should be made
-        daily = 6;
-        monthly = 4;
-        yearly = 20;
-        # hourly = null;
+      templates = {
+        "frequent-backups" = {
+          # how many snapshots should be made
+          daily = 6;
+          monthly = 4;
+          yearly = 20;
+          # hourly = null;
 
-        autosnap = true; # null (default) or boolean
-        autoprune = true; # null (default) or boolean
+          autosnap = true; # null (default) or boolean
+          autoprune = true; # null (default) or boolean
+        };
       };
-
-      # package = pkgs.sanoid;
-      # interval = "hourly"; # how often to run sanoid
 
       # see github page (extra args) and sanoid.defaults.conf
       # settings = {};
       # extraArgs = []; # "--verbose" "--readonly" "--debug"
 
-      datasets."name" = {
-        yearly = 20;
-        monthly = 4;
-        # daily = null;
-        # hourly = null;
+      datasets."name".useTemplate = [ "frequent-backups" ];
 
-        # probably a duplicate
-        # use_template = [];
-        # useTemplate = [];
+      ############
+      # DEFAULTS #
+      ############
 
-        # recursively snapshot dataset children
-        # set to "zfs" to prevent overriding child datasets settings
-        # recursive = false;
-        # process_children_only = false;
-        # processChildrenOnly = false;
+      # datasets."name" = {
+      #   useTemplate = [ ];
 
-        # autosnap = null; # boolean
-        # autoprune = null; # boolean
-      };
+      #   yearly = 20;
+      #   monthly = 4;
+      #   daily = null;
+      #   hourly = null;
+
+      #   # probably a duplicate
+      #   use_template = [];
+
+      #   # recursively snapshot dataset children
+      #   # set to boolean or "zfs" to prevent overriding child datasets settings
+      #   recursive = false;
+      #   process_children_only = false;
+      #   processChildrenOnly = false;
+
+      #   autosnap = null; # boolean
+      #   autoprune = null; # boolean
+      # };
+
+      # package = pkgs.sanoid;
+      # interval = "hourly"; # how often to run sanoid
     };
   };
 }
