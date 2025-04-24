5460c617457ee9c8d0209100a4671343d3729106
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-04-24 15:01:28 +0200

    u-he script converted hash map to list

--- pkgs/u-he/install.sh
+++ pkgs/u-he/install.sh
@@ -6,53 +6,35 @@ if [[ $# -ne 1 || "$1" == "-h" || "$1" == "--help" ]]; then
 fi
 
 PLUGIN=$1
-declare -A plugins=(
-    ["ace"]="ace"
-    ["bazille"]="bazille"
-    ["colourcopy"]="colourcopy"
-    ["diva"]="diva"
-    ["filterscape"]="filterscape"
-    ["hive"]="hive"
-    ["hive"]="hive"
-    ["mfm2"]="mfm2"
-    ["podolski"]="podolski"
-    ["presswerk"]="presswerk"
-    ["repro"]="repro"
-    ["satin"]="satin"
-    ["triple-cheese"]="triple-cheese"
-    ["twangstrom"]="twangstrom"
-    ["tyrell"]="tyrell"
-    ["uhbik"]="uhbik"
-    ["zebra-legacy"]="zebra-legacy"
-    ["zebralette3"]="zebralette3"
+plugins=(
+    "ace"
+    "bazille"
+    "colourcopy"
+    "diva"
+    "filterscape"
+    "hive"
+    "mfm2"
+    "podolski"
+    "presswerk"
+    "repro"
+    "satin"
+    "triple-cheese"
+    "twangstrom"
+    "tyrell"
+    "uhbik"
+    "zebra-legacy"
+    "zebralette3"
 )
 
-if [ ! -v plugins[$PLUGIN] ]; then
-    echo "Plugin \"${PLUGIN}\" not recognised. Use one of these instead:"
+echo "Received $PLUGIN"
+if [[ ! " ${plugins[@]} " =~ " $PLUGIN " ]]; then
+	echo "Plugin \"${PLUGIN}\" not recognised. Use one of these instead:"
 
-    options=("ace"
-	     "bazille"
-	     "colourcopy"
-	     "diva"
-	     "filterscape"
-	     "hive"
-	     "mfm2"
-	     "podolski"
-	     "presswerk"
-	     "repro"
-	     "satin"
-	     "triple-cheese"
-	     "twangstrom"
-	     "tyrell"
-	     "uhbik"
-	     "zebra-legacy"
-	     "zebralette3")
+	for plugin in " ${plugins[@]} "; do
+		echo "=> $plugin"
+	done
 
-    for plugin in "${options[@]}"; do
-	echo "=> $plugin"
-    done
-
-    exit 1
+	exit 1
 fi
 
 nix-build -A "$PLUGIN"
@@ -63,13 +45,13 @@ if [ $PLUGIN == "zebra-legacy" ]; then
     SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
     (cd ./build/lib/*/01*/Zebra2-* && \
-	 nix-shell $SCRIPT_DIR/shell.nix --run "./install.sh --quiet")
+		 nix-shell $SCRIPT_DIR/shell.nix --run "./install.sh --quiet")
 
     (cd ./build/lib/*/02*/ZebraHZ-* && \
-	 nix-shell $SCRIPT_DIR/shell.nix --run "./install.sh --quiet")
+		 nix-shell $SCRIPT_DIR/shell.nix --run "./install.sh --quiet")
 
     mv -vn ./build/lib/*/03* ~/Downloads/
-    
+
     echo "Zebra2 and ZebraHZ installed. Soundsets moved to ~/Downloads/"
 else
     nix-shell ./shell.nix --run "./build/lib/install.sh --quiet"
