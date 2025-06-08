f3735936eb2fa00e5045e62aa863a3e7f8829334
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-06-08 20:13:37 +0200

    vcv rack added for later

--- users/music.nix
+++ users/music.nix
@@ -63,6 +63,7 @@ in {
       # odin2
       # surge-XT # surge with some extras
       cardinal # vcv rack as vst
+      # vcv-rack # needs manual installation of plugins
 
       # small gui. needs to be compiled after tweaking these values:
       # ZynAddSubFXUI() : UI(1181, 659) and z.zest_resize(z.zest, width, height);
