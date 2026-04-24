368cb43a372916d52eda3388793657b1116eeb81
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2026-04-24 14:29:53 +0200

    Filter security tools worth keeping

--- users/packages/security.nix
+++ users/packages/security.nix
@@ -13,39 +13,45 @@ in {
     users.users.${user}.packages = with pkgs; [
       # cli
       nmap # network discovery/auditing
-      netcat-gnu # read network i/o
+      # netcat-gnu # read network i/o. nc already available
       tcpdump # network sniffer
+      mitm6 # network spoofing
+      # fping # faster ping with multiple targets
 
       # toolkits
-      autopsy # sleuth kit
-      ghidra # reverse engineering
+      # autopsy # sleuth kit gui
+      # ghidra # reverse engineering. decompiles to c
       metasploit
       armitage # metasploit frontend
       wireshark # packet sniffer
-      burpsuite # web app testing
+      burpsuite # web app testing. layer 7 man in the middle
 
-      # password cracking
-      john # try johnny for gui
+      # password recovery
+      # john # try johnny for gui frontend
+      # hashcat
 
       # forensic
-      bulk_extractor # file system information extraction
+      # bulk_extractor # file system information extraction
+      # sleuthkit # disk images/data recovery
 
-      # intelligence
-      # spiderfoot # not in nixpkgs but can be installed manually
-      amass # dns enumeration + network mapping
+      # intelligence (dns enumeration/network mapping)
+      # amass
+      # subfinder # faster than amass
+      # # sublist3r # can be installed from source
+      # # spiderfoot # can be installed from source
 
       # wifi/network
       aircrack-ng # security audit
       recon-ng # web reconnaissance
       bettercap # man in the middle
       netexec # crackmapexec fork. exploits
-      responder # man in the middle
+      # responder # man in the middle. insanely comprehensive
 
-      # web app
+      # web app/server
       sqlmap # injection
-      nikto # web server scanner
-      whatweb # web server scanner
-      ffuf # web fuzzing
+      nikto # web server scanner. plugins
+      # whatweb # web server scanner. many plugins
+      # ffuf # web fuzzing. flags like curl. FUZZ from file
     ];
   };
 }
